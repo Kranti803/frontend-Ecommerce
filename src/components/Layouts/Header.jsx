0c3561a3e865d61c7749a3d611d2bdc2b0b7c1bd
Kranti Kumar Shah <Krantikumar803@gmail.com>
2023-12-12 14:53:30 +0545

    first commit

---
+++ src/components/Layouts/Header.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FaRegHeart } from "react-icons/fa";
+import { IoIosSearch, IoMdClose } from "react-icons/io";
+import { BsCart3 } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { navLinks } from "../../constants";
+
+const Header = () => {
+  const isAuthenticated = true;
+  const [nav, setNav] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+      if (screenWidth > 768) setNav(false);
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [screenWidth]);
+
+  return (
+    <nav className="flex items-center justify-between h-[70px] px-1 md:px-4 border-1 border-[#9c9c9cc4] border-b-[1px]">
+      <h2 className=" hidden md:block text-[18px] md:text-[24px] font-[700] font-[Inter]">
+        DazzleMart
+      </h2>
+      <button onClick={() => setNav(true)} className="text-black md:hidden">
+        <GiHamburgerMenu size={25} />
+      </button>
+      <ul className="hidden md:flex gap-8 font-[poppins]">
+        {navLinks.map((item) => (
+          <li key={item.id} className="text-[16px]">
+            <Link to={item.link}>{item.name}</Link>
+          </li>
+        ))}
+        {isAuthenticated ? (
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+        ) : (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+      </ul>
+      <aside className="flex gap-4">
+        <div className="flex gap-1 rounded-sm bg-[#F5F5F5] p-2">
+          <input
+            className=" text-xs outline-none bg-transparent"
+            type="text"
+            placeholder="What are you looking for ?"
+          />
+          <button>
+            <IoIosSearch />
+          </button>
+        </div>
+        <button>
+          <FaRegHeart size={20} />
+        </button>
+        <button>
+          <BsCart3 size={20} />
+        </button>
+      </aside>
+
+      {/**************************mobile nav ******************************/}
+      <aside
+        className={`text-white absolute h-full w-[60%] top-0 left-0 bg-black p-4 duration-200 ease-in ${
+          nav ? "translate-x-[0%]" : "translate-x-[-150%]"
+        }`}
+      >
+        <div className="flex justify-between">
+          <h2 className="text-xl font-[700] font-[Inter]">DazzleMart</h2>
+          <button onClick={() => setNav(false)}>
+            <IoMdClose size={25} />
+          </button>
+        </div>
+        <ul className=" text-lg flex flex-col gap-14 py-8 font-[poppins]">
+          {navLinks.map((item) => (
+            <li onClick={() => setNav(false)} key={item.id}>
+              <Link to={item.link}>{item.name}</Link>
+            </li>
+          ))}
+          {isAuthenticated ? (
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          )}
+        </ul>
+      </aside>
+      {/**************************mobile nav ******************************/}
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/constants.js
@@ -0,0 +1,19 @@
+export const navLinks = [
+    {
+        id: 1,
+        name: "Home",
+        link: "/",
+    },
+    {
+        id: 2,
+        name: "About",
+        link: "/about"
+    },
+    {
+        id: 3,
+        name: "Contact",
+        link: "/about",
+    },
+
+
+];
