d7f6c50b978dfddec9bdc9fcf28a0f52688dc9a5
Kranti Kumar Shah <Krantikumar803@gmail.com>
2024-06-12 18:17:31 +0545

    fixed home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import HeroCarousel from './HeroCarousel';
-import FeaturedProducts from './FeaturedProducts';
-import ExploreProducts from './ExploreProducts';
-import Banner from './Banner';
-import Services from './Services';
+import React from "react";
+import HeroCarousel from "./HeroCarousel";
+import FeaturedProducts from "./FeaturedProducts";
+import ExploreProducts from "./ExploreProducts";
+import Banner from "./Banner";
+import Services from "./Services";
+import { useSelector } from "react-redux";
+import Loader from "../Layouts/Loader";
 
 const Home = () => {
+  const { loading } = useSelector((state) => state.auth);
+  if (loading) return <Loader />;
   return (
-    <section className='md:w-[80%] m-auto'>
-        <HeroCarousel/>
-        <FeaturedProducts/>
-        <Banner/>
-        <ExploreProducts/>
-        <Services/>
+    <section className="md:w-[80%] m-auto">
+      <HeroCarousel />
+      <FeaturedProducts />
+      <Banner />
+      <ExploreProducts />
+      <Services />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
